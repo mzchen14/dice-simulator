2db277117194a4789467e4ab93e82034a6eb0ae0
Ming Chen <mzchen14@gmail.com>
2020-06-09 18:12:59 -0400

    Added wobbling animation to dice roll

--- src/Die.js
+++ src/Die.js
@@ -3,7 +3,9 @@ import "./Die.css"
 
 class Die extends Component {
     render() {
-        return <i className={`Die fas fa-dice-${this.props.face}`}/>
+        return <i className={`Die fas fa-dice-${this.props.face} ${this.props.rolling  && 'shaking'}`}/>
     }
 }
 export default Die 
+
+//Note this.props.rolling  && 'shaking' is the same as this.props.roll ? shaking : ''
